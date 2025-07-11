0e05443905758a841ee118036771e0819d3795b8
Sahrul Gunawan Cyber <developer.limited.id@gmail.com>
2025-07-11 12:54:53 +0700

    Update LOOP.bat

--- MODULE/LOOP.bat
+++ MODULE/LOOP.bat
@@ -1,8 +1,15 @@
 @echo off
-echo RDP CREATION SUCCESSFULL!
-tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 >Nul & exit
+echo RDP CREATION SUCCESSFUL!
+tasklist | find /i "ngrok.exe" >nul && goto check || (
+    echo Unable to get NGROK tunnel. Make sure NGROK_AUTH_TOKEN is correct in Secrets.
+    echo Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels
+    ping 127.0.0.1 >nul
+    exit
+)
+
 :check
-ping 127.0.0.1 > null
 cls
-echo RDP CREATION SUCCESSFULL!
+echo RDP CREATION SUCCESSFUL!
+echo Time: %TIME%   Date: %DATE%
+timeout /t 3 >nul
 goto check
