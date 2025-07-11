4545e090d64083c08991a5f058b12f629dd286e2
Sahrul Gunawan Cyber <developer.limited.id@gmail.com>
2025-07-11 12:58:01 +0700

    Update WALLPAPER.bat

--- MODULE/WALLPAPER.bat
+++ MODULE/WALLPAPER.bat
@@ -1,25 +1,34 @@
 @echo off
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (
-echo Looking for Admin Access!
-goto UACPrompt
-) else ( goto Meet Admin )
+    echo 🔒 Looking for Admin Access...
+    goto UACPrompt
+) else (
+    goto MeetAdmin
+)
+
 :UACPrompt
 echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
 echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
 "%temp%\getadmin.vbs"
 exit /B
-:Meet Admin
-if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
-pushd "%CD%"
+
+:MeetAdmin
+if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs"
 CD /D "%~dp0"
+
+REM 🖥️ Ubah branding & identitas
 REG ADD HKLM\SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName /v ComputerName /t REG_SZ /d Oshekher /f
-REG ADD HKLM\SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName\ /v ComputerName /t REG_SZ /d Oshekher /f
-REG ADD HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\ /v Hostname /t REG_SZ /d Oshekher /f
-REG ADD HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\ /v "AD Host" /t REG_SZ /d AdityaRDP /f
+REG ADD HKLM\SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName /v ComputerName /t REG_SZ /d Oshekher /f
+REG ADD HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v Hostname /t REG_SZ /d Oshekher /f
+REG ADD HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters /v "AD Host" /t REG_SZ /d AdityaRDP /f
+
+REM 🏷️ OEM Branding
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v Manufacturer /t REG_SZ /d "AdHost" /f
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v Model /t REG_SZ /d "AdHost Virtual Machine" /f
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v SupportURL /t REG_SZ /d "https://github.com/MarcelCyan/FreeRDP/issues" /f
+
+REM 🧭 Taskbar & Explorer Refresh
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSmallIcons /t REG_DWORD /d 1 /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 taskkill /f /im explorer.exe
